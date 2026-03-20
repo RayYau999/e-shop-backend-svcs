f10814f7f5f3f86d8e13b0e11c060b4ad6b37c01
ray.yau <a05166ray@gmail.com>
2026-03-21 01:26:50 +0800

    feat/resilience4j-impl : circuit breaker implementation

--- eshop-api-gateway/src/main/java/com/rayyau/eshop/eshop/api/gateway/controller/LoginController.java
+++ eshop-api-gateway/src/main/java/com/rayyau/eshop/eshop/api/gateway/controller/LoginController.java
@@ -6,9 +6,12 @@ import com.rayyau.eshop.security.library.dto.LoginResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @AllArgsConstructor
@@ -16,9 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     private final LoginService loginService;
 
+    @CircuitBreaker(name = "loginService", fallbackMethod = "fallbackResponse")
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Valid LoginRequest request) {
         log.info("login running from api gateway controller");
         return loginService.login(request);
+    }
+
+    // 這是你的 Fallback 方法
+    public LoginResponse fallbackResponse(LoginRequest request, Throwable e) {
+        log.error("觸發斷路器或服務異常！原因: {}", e.getMessage());
+
+        Date currentDate = new Date();
+
+        // 預設的備援回覆
+        return new LoginResponse("服務暫時不可用，請稍後再試。", currentDate, currentDate);
     }
 }
