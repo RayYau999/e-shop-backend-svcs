f690b961ebd54c298fa4ec13d7733b18874668e5
ray.yau <a05166ray@gmail.com>
2026-03-21 15:04:27 +0800

    feat/resilience4j-impl: add resilience retry logic to login api

--- eshop-api-gateway/src/main/java/com/rayyau/eshop/eshop/api/gateway/controller/LoginController.java
+++ eshop-api-gateway/src/main/java/com/rayyau/eshop/eshop/api/gateway/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.rayyau.eshop.eshop.api.gateway.controller;
 import com.rayyau.eshop.eshop.api.gateway.service.LoginService;
 import com.rayyau.eshop.security.library.dto.LoginRequest;
 import com.rayyau.eshop.security.library.dto.LoginResponse;
+import io.github.resilience4j.retry.annotation.Retry;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,20 +20,9 @@ import java.util.Date;
 public class LoginController {
     private final LoginService loginService;
 
-    @CircuitBreaker(name = "loginService", fallbackMethod = "fallbackResponse")
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Valid LoginRequest request) {
         log.info("login running from api gateway controller");
         return loginService.login(request);
-    }
-
-    // 這是你的 Fallback 方法
-    public LoginResponse fallbackResponse(LoginRequest request, Throwable e) {
-        log.error("觸發斷路器或服務異常！原因: {}", e.getMessage());
-
-        Date currentDate = new Date();
-
-        // 預設的備援回覆
-        return new LoginResponse("服務暫時不可用，請稍後再試。", currentDate, currentDate);
     }
 }
